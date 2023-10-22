26894392b49c296c3b67430123893d7afee8374f
loitdbw <129943395+loitdbw@users.noreply.github.com>
2023-10-22 11:12:20 +0700

    modify again dbconnect to deloy

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const dbConnect = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log('DB connected successfully')
-    } catch(error){
-        console.log("DB connected failed", error.message)
-    }
-}
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
 
-dbConnect()
+dbConnect();
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const http = require("http");
 require("./config/dbConnect");
 const app = require("./app/app");
 
 const PORT = process.env.PORT || 2020;
 
-//server
-const server = http.createServer(app);
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
